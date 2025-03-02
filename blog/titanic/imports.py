36f786b4df942e05b2b51901efee8ad283970f37
rachidsahli <rachidsahli144@gmail.com>
2025-03-02 23:23:29 +0100

    Modif

--- blog/titanic/imports.py
+++ blog/titanic/imports.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
+import matplotlib.colors as mcolors
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 
