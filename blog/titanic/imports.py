4392e221329d6e679d92e429d8f0ced590778547
rachidsahli <rachidsahli144@gmail.com>
2025-04-21 23:34:35 +0200

    Rafraichissement

--- blog/titanic/imports.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import plotly.express as px
-import matplotlib.colors as mcolors
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-
